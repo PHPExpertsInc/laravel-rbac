e8c81562b578f250f4431729ebecda79483b4564
Tim Helfensdörfer <tim@visualappeal.de>
2018-01-24 23:41:48 +0100

    Added isnt operator

--- src/Middleware/Rbac.php
+++ src/Middleware/Rbac.php
@@ -15,13 +15,16 @@ class Rbac
      */
     public function handle($request, Closure $next, $level, $permission)
     {
-        if(!in_array($level, ['is', 'can']))
+        if (!in_array($level, ['is', 'isnt', 'can']))
             abort(500, 'Invalid RBAC operator specified.');
-        if('is' == $level) {
-            if($request->user()->hasRole($permission))
+        if ('is' == $level) {
+            if ($request->user()->hasRole($permission))
+                return $next($request);
+        } else if ('isnt' == $level) {
+            if (!$request->user()->hasRole($permission))
                 return $next($request);
         } else {
-            if($request->user()->canDo($permission))
+            if ($request->user()->canDo($permission))
                 return $next($request);
         }
 
