1c6b6aa72d5501d7adff1d622c1e4ca22f9e2f68
Theodore R. Smith <theodore@phpexperts.pro>
2018-12-19 16:07:43 -0600

    Enable Laravel autopackage discovery.

--- src/RbacServiceProvider.php
+++ src/RbacServiceProvider.php
@@ -6,6 +6,15 @@ use Blade;
 
 class RbacServiceProvider extends ServiceProvider
 {
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['rbac'];
+    }
 
     /**
      * Indicates if loading of the provider is deferred
