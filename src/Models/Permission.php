6dab8468befec81a44b3fc8fafc97de8f05724b6
Theodore R. Smith <theodore@phpexperts.pro>
2019-04-26 07:31:08 -0500

    Documented the models.

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -2,13 +2,23 @@
 
 namespace PHPExperts\LaravelRBAC\Models;
 
+use Carbon\Carbon;
 use PHPExperts\ConciseUuid\ConciseUuid;
 
+/**
+ * @property string $name
+ * @property string $slug
+ * @property string $description
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ */
 class Permission extends ConciseUuid
 {
     protected $table = 'rbac_permissions';
 
     protected $fillable = ['name', 'slug', 'description'];
+
+    protected $dates = ['created_at', 'updated_at'];
 
     public function roles()
     {
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,13 +2,23 @@
 
 namespace PHPExperts\LaravelRBAC\Models;
 
+use Carbon\Carbon;
 use PHPExperts\ConciseUuid\ConciseUuid;
 
+/**
+ * @property string $name
+ * @property string $slug
+ * @property string $description
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ */
 class Role extends ConciseUuid
 {
     protected $table = 'rbac_roles';
 
     protected $fillable = ['name', 'slug', 'description'];
+
+    protected $dates = ['created_at', 'updated_at'];
 
     public function users()
     {
